232dd9025616ddb268e145f17ab8326677a96c30
KirillBunakov <lonley987@gmail.com>
2023-10-05 11:44:06 +0300

    Complited the task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string[] arr = new string[length];
+
+for (int i = 0; i < arr.Length; i++)
+{
+  Console.Write("Введите элемент массива: ");
+  arr[i] = Console.ReadLine();
+}
+Console.WriteLine(String.Join(",", arr));
+
+
+string[] resultArray = new string[arr.Length];
+int realSize = 0;
+foreach (var value in arr)
+{
+  if (value.Length <= 3)
+  {
+    resultArray[realSize] = value;
+    Console.Write(resultArray[realSize] + " ");
+    realSize++;
+  }
+}
+
+
